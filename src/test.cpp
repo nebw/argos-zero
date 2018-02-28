ea95d0c154b1b4128b744242ecded4fb1938a69f
cashmash <buiss.senn@googlemail.com>
2018-02-28 13:57:41 +0100

    [#4] tests logFilePath setter/getter from ArgosConfig singleton

--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,9 @@
 #include "argos/Config.h"
 
 int main() {
-    ArgosConfig::get().networkPath("test/path");
+    ArgosConfig::get().networkPath("test/networkPath");
+    ArgosConfig::get().logFilePath("test/logFilePath");
+
     std::cout << ArgosConfig::get().networkPath() << std::endl;
+    std::cout << ArgosConfig::get().logFilePath() << std::endl;
 }
