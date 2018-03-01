0491ac45c32f41655e19f76479c5f06f28c8644a
franziska93 <franziska93@mi.fu-berlin.de>
2018-03-01 14:29:36 +0100

    Added functionality to fill Capnp messages during selfplay.

--- src/selfplay.cpp
+++ src/selfplay.cpp
@@ -18,7 +18,13 @@
 //capnp
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
-#include "capnp/Message.h"
+#include "capnp/CapnpGame.h"
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <ctime>
 
 int main() {
 
@@ -26,9 +32,9 @@ int main() {
     // if yes, where to send them (IP Adress:port)
     bool collect_data = 0;
 
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    string server_name;
+//    int sockfd, portno, n;
+//    struct sockaddr_in serv_addr;
+//    string server_name;
 
     Tree tree;
     tree.setKomi(7.5);
@@ -69,7 +75,7 @@ int main() {
         if (collect_data){
 
             std::array<double, 362> node_probs; // an allen Stellen mit 0 initialisieren
-            std::int8_t pos, row, col, board_size; //where in the array to write the probability
+            std::int16_t pos, row, col, board_size; //where in the array to write the probability
 
             // iterate over all node. only legal moves do exist.
             for (auto node : tree.rootNode()->children().get())
@@ -125,42 +131,86 @@ int main() {
 
     if (collect_data){
 
-        //num_moves = len(x);
+        std::uint16_t a, b, i, j, k;
+        std::uint16_t num_moves = states.size();
+
+        std::uint16_t num_flattened_features = board_size * board_size * 8; // try to replace hardcoding of 8!
 
         ::capnp::MallocMessageBuilder message;
-        Game::Builder game = message.initRoot<Game>(2);
+        Game::Builder game = message.initRoot<Game>();
+        ::capnp::List<StateProb>::Builder stateprobs = game.initStateprobs(num_moves);
 
+
+        // fill the StateProb structs with the information from the vectors
+        for (a=0;a<num_moves;a++){
+            StateProb:: Builder stateprob = stateprobs[a];
+            stateprob.setIdx(a);
+
+            // fill probs
+            ::capnp::List<float>::Builder probs =  stateprob.initProbs(362);
+            for (b=0;b<362;b++){
+                probs.set(b, probabilities[a][b]);
+            }
+
+            //fill states
+            ::capnp::List<float>::Builder cstates =  stateprob.initState(num_flattened_features);
+            for (i=0;i<board_size;i++){
+                for (j=0;j<board_size;j++){
+                    for (k=0;k<8;k++){
+                        int ind = board_size * 8 * i + 8 * j + k;
+                        cstates.set(ind, states[a][i][j][k]);
+                    }
+                }
+            }
+
+        }
+
+        // now fill the game
+
+        boost::uuids::uuid id(boost::uuids::random_generator());
+        game.setId(boost::lexical_cast<std::string>(id));
+
+        std::time_t time = std::time(nullptr);
+        game.setTimestamp(time);
+
+        game.setBoardsize(board_size);
+
+        game.setNetwork1(0); // to be initialized with the correct network idea
+        game.setNetwork2(0);
+
+
+//        game.setResult();
 
 
 
         // fill the game struct with all information collected
         // open TCP socket
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0)
-                error("ERROR opening socket");
+//        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//        if (sockfd < 0)
+//                error("ERROR opening socket");
 
-        server = gethostbyname(server_name);
+//        server = gethostbyname(server_name);
 
-        if (server == NULL) {
-                fprintf(stderr,"ERROR, no such host\n");
-                exit(0);
-            }
-        serv_addr.sin_family = AF_INET;
+//        if (server == NULL) {
+//                fprintf(stderr,"ERROR, no such host\n");
+//                exit(0);
+//            }
+//        serv_addr.sin_family = AF_INET;
 
-        bcopy((char *)server->h_addr,
-                 (char *)&serv_addr.sin_addr.s_addr,
-                 server->h_length);
+//        bcopy((char *)server->h_addr,
+//                 (char *)&serv_addr.sin_addr.s_addr,
+//                 server->h_length);
 
-        serv_addr.sin_port = htons(portno);
+//        serv_addr.sin_port = htons(portno);
 
-        if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-                error("ERROR connecting");
+//        if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+//                error("ERROR connecting");
 
-        // write capnp message to socket
-        writePackedMessageToFd(sockfd, message);
+//        // write capnp message to socket
+//        writePackedMessageToFd(sockfd, message);
 
-        //close socket
-        close(sockfd);
+//        //close socket
+//        close(sockfd);
     }
 
 }
