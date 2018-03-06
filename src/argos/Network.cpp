4a02438002b0171b39114693b51922e260e82ab7
Benjamin Wild <b.w@fu-berlin.de>
2018-03-06 16:25:28 +0100

    avoid pass moves for now

--- src/argos/Network.cpp
+++ src/argos/Network.cpp
@@ -66,7 +66,7 @@ std::array<Network::Result, config::tree::batchSize> Network::apply(
             }
         }
         //candidates.emplace_back(policyOutput[width * height], Vertex::Pass());
-        candidates.emplace_back(1e-8, Vertex::Pass());
+        candidates.emplace_back(0.0000001f, Vertex::Pass());
         results[i] = Network::Result(std::move(candidates), valueOutput[i] * 2 - 1);
     }
     return results;
