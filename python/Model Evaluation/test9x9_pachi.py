495124bbf06740e95359a7fa935cda3401a2b168
Abdullah Barhoum <ka70911@gmail.com>
2018-03-07 13:36:43 +0100

    Model Evaluation: updated test examples

--- python/Model Evaluation/test9x9_pachi.py
+++ python/Model Evaluation/test9x9_pachi.py
@@ -5,9 +5,9 @@ from ratingsystem import RatingSystem
 # pretrained random version of our model is needed or the gtp file in the build folder
 numMatches = 1
 print "running " + str(numMatches)+ " matches"
-print "will use the default ratings available in ratings.txt"
+print "will use the default ratings available in ./rating/ratings# file"
 playerOneParams = ["./argos_model/gtp", "-p",
                    "./argos_model/agz_small_9x9", "-l", "./argos_model/agz_small_9x9.log"]
 playerTwoParams = ["./pachi", "--no-dcnn", "-e", "uct", "-D"]
-test = RatingSystem(playerOneParams, playerTwoParams, numMatches=numMatches)
-test.run()
+test = RatingSystem(playerOneParams, playerTwoParams, numMatches=numMatches, filePath ="./rating")
+print test.run()
