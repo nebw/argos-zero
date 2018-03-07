fdad15b7fa9828cc6b0297d8facd40078b6aa6a3
franziska93 <franziska93@mi.fu-berlin.de>
2018-03-07 11:02:46 +0100

    Restore old config state that was lost during merge conflict resolve.

--- src/argos/Config.h
+++ src/argos/Config.h
@@ -260,3 +260,45 @@ namespace argos {
     }
 }
 
+// TODO deprecated, use config object instead and delete unused parameters
+namespace config {
+
+    enum MXNET_DEVICE_TYPE {
+        CPU = 1, GPU = 2, CPU_PINNED = 3
+    };
+
+    static const MXNET_DEVICE_TYPE defaultDevice = CPU;
+
+    //static const boost::filesystem::path networkPath("/Users/rs/Documents/dev/uni/swpdeeplearning/tmp/expertnet_small");
+    //static const boost::filesystem::path logFilePath("/Users/rs/Documents/dev/uni/swpdeeplearning/tmp/argos-dbg.log");
+
+    static const size_t boardSize = BOARDSIZE;
+    static const char* server = "127.0.0.1";
+    static const int port = 8000;
+
+    namespace tree {
+        static const size_t batchSize = 8;
+        static const size_t numEvaluationThreads = 2;
+        static const size_t numThreads = std::max<size_t>(
+                numEvaluationThreads * batchSize,
+                std::thread::hardware_concurrency() == 0 ? 4 : std::thread::hardware_concurrency());
+        static const size_t randomizeFirstNMoves = 10;
+        static const size_t numLastRootNodes = 3;
+        static const size_t virtualPlayouts = 5;
+        static const size_t expandAt = virtualPlayouts + 1;
+        static const float priorC = 5;
+        static const bool networkRollouts = false;
+        static const bool trainingMode = true;
+    }  // namespace tree
+
+    namespace time {
+        static const int C = 80;
+        static const int maxPly = 80;
+        static const auto delay = std::chrono::milliseconds(10);
+    }  // namespace time
+
+    namespace engine {
+        static const auto totalTime = std::chrono::milliseconds(1000 * 60 * 10);
+        static const float resignThreshold = 0.1f;
+    }  // namespace engine
+}  // namespace config
